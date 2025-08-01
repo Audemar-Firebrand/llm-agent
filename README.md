2bb7fc9816b7890f210b2ec6a5256091f365a112
Audemar-Firebrand <19tristan.martin@gmail.com>
2025-07-31 23:35:11 -0600

    added a 'get info' function and test cases to manually debug

---
+++ README.md
@@ -0,0 +1,1 @@
+LLM project made with boot.dev
---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+def get_files_info(
+    working_directory, directory="."):
+    full_path = os.path.join(working_directory, directory)
+    abs_full_path = os.path.abspath(full_path)
+    abs_working_path = os.path.abspath(working_directory)
+    if not abs_full_path.startswith(abs_working_path):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    elif not os.path.isdir(abs_full_path):
+        return f'Error: "{directory}" is not a directory'
+    else:
+        try:
+            contents = [] 
+            dir_list = os.listdir(abs_full_path)
+
+            for item in dir_list:
+                file_size = os.path.getsize(os.path.join(abs_full_path, item))
+                is_dir = os.path.isdir(os.path.join(abs_full_path, item))
+                file_string = f"- {item}: file_size={file_size}, is_dir={is_dir}"
+                contents.append(file_string)
+            return "\n".join(contents)
+        except Exception as e:
+            return f"Error: {str(e)}"
