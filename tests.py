65404b3c33e83bae4c0daafda40ce1fd51bea0cc
Audemar-Firebrand <19tristan.martin@gmail.com>
2025-08-04 21:56:35 -0600

    added a file contents function and debugged

--- tests.py
+++ tests.py
@@ -1,19 +1,12 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-result1 = get_files_info("calculator", ".")
-print("Result for current directory:")
+result1 = get_file_content("calculator", "main.py")
 print(result1)
-    
-result2 = get_files_info("calculator", "pkg")
-print("Result for 'pkg' directory:")
+
+result2 = get_file_content("calculator", "pkg/calculator.py")
 print(result2)
 
-result3 = get_files_info("calculator", "/bin")
-print("Result for '/bin' directory:")
+result3 = get_file_content("calculator", "/bin/cat")
 print(result3)
 
-result4 = get_files_info("calculator", "../")
-print("Result for '../' directory:")
-print(result4)
-
-
+result4 = get_file_content("calculator", "pkg/does_not_exist.py")
