fb37e16ad642b93bb8523eae4eff2afe728fccb9
Audemar-Firebrand <19tristan.martin@gmail.com>
2025-08-12 20:09:06 -0600

    added ability to agent to 'call' functions from an available list

--- main.py
+++ main.py
@@ -3,10 +3,26 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 from google import genai
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = types.Tool(
+    function_declarations=[schema_get_files_info,
+    ]
+)
 
 client = genai.Client(api_key=api_key)
 def main():
@@ -26,9 +42,12 @@ def main():
 ]
     print("Hello from llm-agent!")
     response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
 )
+    if response.function_calls:
+        print(f"Calling function: {response.function_calls[0].name}({response.function_calls[0].args})")
     print(response.text)
     if args.verbose:
         print(f"User prompt: {user_prompt}")
